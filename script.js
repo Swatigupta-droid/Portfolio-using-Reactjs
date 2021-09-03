c3015f96715138756b86cefb718e0447fe0dece5
Swati Gupta <64798478+Swatigupta-droid@users.noreply.github.com>
2021-09-04 03:14:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function (){
+    $("h1").fadeIn(1000,function(){
+        $("h2").fadeIn(1000);
+    });
+    $("div").fadeIn(1500,function(){
+        $("section").fadeIn(1500);
+    });
+});
+
+
